0f87cb0d27c11604db9f00c5afa184ee077c411b
omidk414 <56232795+omidk414@users.noreply.github.com>
2021-05-19 11:32:37 -0700

    Add files via upload

---
+++ Arduino.ino
@@ -0,0 +1,24 @@
+int LED = 13 ; // define LED Interface
+int Shock = 3; // define the percussion Sensor Interface
+int val ; // define numeric variables val
+
+void setup ()
+{
+  pinMode (LED, OUTPUT) ; // define LED as output interface
+  //pinMode (Shock,INPUT) ; // define knock sensor output inferface
+  Serial.begin(9600);
+  }
+ void loop ()
+{
+  delay(5000);
+  val = analogRead(Shock) ; // read digital interface is assigned a value of 3 val
+  Serial.println(val);
+  if (val == HIGH) // When the percussion when the sensor detects a signal, LED flashes
+  {
+    digitalWrite (LED, LOW);
+  }
+  else
+  {
+    digitalWrite (LED, HIGH);
+  }
+}
